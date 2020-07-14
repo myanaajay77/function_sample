a431002c6450cf87e247e2865818045613d776ee
myanaajay77 <myana.ajay77@gmail.com>
2020-07-14 15:05:21 +0530

    initial commit

---
+++ pipline_setup.py
@@ -0,0 +1,17 @@
+def verify(num1,num2):
+    if num1 > num2:
+        return num1
+    elif num1 == num2:
+        return 1
+    else:
+        return num2
+
+def display(arg1,arg2):
+    if(verify(arg1,arg2)==arg1):
+        print("A")
+    elif(verify(arg1,arg2)==1):
+        print("C")
+    else:
+        print("B")
+        
+display(1000,3500) 
